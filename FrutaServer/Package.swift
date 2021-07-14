b559e9449679912e62e7b904319a1b0e50060f5e
Leo Dion <leogdion@brightdigit.com>
2021-07-14 13:25:00 -0400

    adding FrutaKit

--- FrutaServer/Package.swift
+++ FrutaServer/Package.swift
@@ -10,6 +10,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+      .library(name: "FrutaKit", targets: ["FrutaKit"]),
         .library(
             name: "FrutaServer",
             targets: ["FrutaServer"]),
@@ -24,6 +25,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+      .target(name: "FrutaKit"),
         .target(
             name: "FrutaServer",
             dependencies: [
